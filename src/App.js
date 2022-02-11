ae62f2d4e4a7e3722bcca5c99be3ca6e4885b509
alpagu872 <83309872+alpagu872@users.noreply.github.com>
2022-02-11 17:26:27 +0300

    state giris

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
 
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  Button
+  SafeAreaView,  StyleSheet,Text,View, Button
 } from 'react-native';
 
 function App() {
+const [counter, setCounter] = useState(0);
+
+function increaseCounter(){
+  setCounter(counter + 1);
+}
+function decreaseCounter(){
+  setCounter(counter - 1);
+}
 
 
-  const [counter, setCounter] = useState(0);
-
-  function updateCounter(){
-    setCounter(counter+1);
-  }
-  function downCounter(){
-    setCounter(counter-1);
-  }
-
-
-
-
-  return(
+  return (
     <SafeAreaView>
-      <Text style = {{fontSize:40}}>Counter: {counter}</Text>
-      <Button title= "Update Counter" onPress={updateCounter}/>
-      <Button title= "Down Counter" onPress={downCounter}/>
+      <Text style={{ fontSize: 40, margin : 10 }}>Counter: {counter} </Text>
+      <Button title="Increase Counter" onPress={increaseCounter} />
+      <Button title = "Decrease Counter" onPress={decreaseCounter}/>
 
     </SafeAreaView>
   )
