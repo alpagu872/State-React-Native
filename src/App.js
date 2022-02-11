f3a357a1b135f0385469bc591c47d9762dc43825
alpagu872 <83309872+alpagu872@users.noreply.github.com>
2022-02-11 16:47:16 +0300

    src folder

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App() {
+  return(
+    <SafeAreaView>
+      <Text>Hİ</Text>
+    </SafeAreaView>
+  )
+}
+
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
