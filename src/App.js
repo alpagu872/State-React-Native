5e02642291fc184a67dee42a560e854fa930858a
alpagu872 <83309872+alpagu872@users.noreply.github.com>
2022-02-11 18:46:05 +0300

    state devam update

--- src/App.js
+++ src/App.js
@@ -3,25 +3,46 @@
 import React, { useState } from 'react';
 
 import {
-  SafeAreaView,  StyleSheet,Text,View, Button
+  SafeAreaView, StyleSheet, Text, View, Button, FlatList, Switch
 } from 'react-native';
 
-function App() {
-const [counter, setCounter] = useState(0);
+const data = [
+  { id: 0, name: 'cafe.exe', isFavorite: true },
+  { id: 1, name: 'KafaKafe', isFavorite: false },
+  { id: 2, name: 'BugG', isFavorite: false },
+  { id: 3, name: 'Rock`n Code', isFavorite: true },
+  { id: 4, name: 'do(drink)', isFavorite: false },
+  { id: 5, name: 'esc', isFavorite: false },
+]
 
-function increaseCounter(){
-  setCounter(counter + 1);
-}
-function decreaseCounter(){
-  setCounter(counter - 1);
-}
+function App() {
+
+  const [cafeList, setCafeList] = useState(data);
+  const [showOnlyFavorites, setShowOnlyFavorites] = useState(false)
+
+  function onFavoritesChange(isFavoriteSelected) {
+    setShowOnlyFavorites(isFavoriteSelected);
+    isFavoriteSelected ?
+      setCafeList(cafeList.filter(cafe => cafe.isFavorite))
+      :
+      setCafeList(data);
+
+
+  }
+
 
 
   return (
     <SafeAreaView>
-      <Text style={{ fontSize: 40, margin : 10 }}>Counter: {counter} </Text>
-      <Button title="Increase Counter" onPress={increaseCounter} />
-      <Button title = "Decrease Counter" onPress={decreaseCounter}/>
+      <View style={{ margin: 10 }}>
+        <Text>Show Only Favorites</Text>
+        <Switch value={showOnlyFavorites} onValueChange={onFavoritesChange} />
+        <FlatList data={cafeList} renderItem={({ item }) =>
+          <Text style={{ fontSize: 25 }}>{item.name}</Text>
+        } />
+
+      </View>
+
 
     </SafeAreaView>
   )
